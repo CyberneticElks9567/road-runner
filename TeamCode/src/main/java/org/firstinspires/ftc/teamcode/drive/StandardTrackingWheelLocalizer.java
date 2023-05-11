59e67aeb85ec52527598b5fd9a38082592c9d242
CyberneticElks9567 <55330835+CyberneticElks9567@users.noreply.github.com>
2023-05-10 17:35:52 -0700

    Changed Odometry Settings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -28,10 +28,10 @@ import java.util.List;
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 8192;
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 1.181; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 18; //16.5 // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 16.5; //16.5 // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 9; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
